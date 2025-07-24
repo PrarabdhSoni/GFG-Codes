f1964c6cf869e8a77bd482c676305f4cbaec4f30
Prarabdh Soni <pscs.prarabdhsoni2005@gmail.com>
2025-07-25 00:18:33 +0530

    Adding Array Leader

---
+++ src/ArrayLeader.java
@@ -0,0 +1,41 @@
+//https://www.geeksforgeeks.org/problems/leaders-in-an-array-1587115620/1?page=1&sortBy=submissions
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayLeader {
+    public static ArrayList<Integer> leaders(int[] arr) {
+        // code here
+        ArrayList<Integer> leader = new ArrayList<>();
+
+        int n = arr.length;
+        leader.add(arr[n-1]);
+        int max = arr[n-1];
+
+        for(int i = n-2; i>=0 ; i--){
+            if(max <= arr[i]){
+                leader.add(arr[i]);
+                max = arr[i];
+            }
+        }
+
+
+        int start = 0;
+        int end = leader.size()-1;
+
+        while(start <= end){
+
+            Collections.swap(leader, start, end);
+            start ++;
+            end --;
+        }
+
+        return leader;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {16, 17, 4, 3, 5, 2};
+        leaders(arr);
+    }
+
+}
